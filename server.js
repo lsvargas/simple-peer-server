bc26c94f8e7808e38e220d08dc6199136dc8e386
notfound <lsvargas@uc.cl>
2021-06-07 16:42:43 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const cors = require('cors')
+const { ExpressPeerServer } = require("peer");
+
+const io = require('socket.io')(server, {
+  allowEIO3: true,
+  cors: {
+    origin: 'https://peerjs-videocall.vercel.app/',
+    // origin: 'http://localhost:3001',
+    credentials: true
+  }
+});
+const { v4: uuidV4 } = require('uuid')
+
+app.use(cors());
+
+const customGenerationFunction = () =>
+  (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+
+const peerServer = ExpressPeerServer(server, {
+  path: "/",
+  generateClientId: customGenerationFunction,
+});
+
+app.use("/mypeer", peerServer);
+
+var link
+app.get('/', (req, res) => {
+  link = uuidV4()
+  res.redirect(`/${link}`)
+})
+
+//API para obtener link de la llamada
+app.get('/API', (req, res) => {
+  return res.send(link)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+    // socket.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(3000)
